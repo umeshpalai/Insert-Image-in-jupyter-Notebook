fecf5604f9a1dfca5cbd16f94a5f18e1bfa8fb2f
Umesh Palai <38834371+umeshpalai@users.noreply.github.com>
2019-03-16 16:44:29 +0530

    Add files via upload

---
+++ Insert Image in Jupyter notebook.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# # How to upload/insert - Image/Text 
+# # How to write a headline
+
+# In[1]:
+
+
+# insert Image
+from IPython.display import Image
+Image("Desktop\AI.jpg")
+
+
+# In[4]:
+
+
+#insert image in markdown
+
+
+# ![title](Desktop\AI.jpg)
+
+# ![title](Desktop\AI.jpg)
